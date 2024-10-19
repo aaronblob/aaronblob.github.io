67848bee01cc15ad8545f8a6dee33ee6d11a5313
Aaron Lob <aaron.lob@hotmail.com>
2024-10-19 11:09:58 +0200

    Release version-1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Include the head from your separate PHP file -->
+<?php include 'header.php'; ?>
+<body>
+    <div class="container text-center mt-5">
+        <!-- Profile Image Section -->
+        <div class="profile-image">
+            <img src="graphics/lob.jpg" alt="Picture" class="rounded-circle" style="width: 150px; height: 150px;">
+        </div>
+
+        <!-- Social Icons Section -->
+        <div class="mt-4">
+            <a href="https://scholar.google.com/" target="_blank" class="mx-2">
+                <img src="google-scholar-icon.png" alt="Google Scholar" style="width: 40px; height: 40px;">
+            </a>
+            <a href="https://mastodon.social/" target="_blank" class="mx-2">
+                <img src="mastodon-icon.png" alt="Mastodon" style="width: 40px; height: 40px;">
+            </a>
+            <a href="https://bsky.app/" target="_blank" class="mx-2">
+                <img src="bluesky-icon.png" alt="Bluesky" style="width: 40px; height: 40px;">
+            </a>
+            <a href="https://twitter.com/" target="_blank" class="mx-2">
+                <img src="twitter-icon.png" alt="Twitter" style="width: 40px; height: 40px;">
+            </a>
+            <a href="https://orcid.org/" target="_blank" class="mx-2">
+                <img src="orcid-icon.png" alt="ORCID" style="width: 40px; height: 40px;">
+            </a>
+        </div>
+    </div>
+</body>
+</html>
+
